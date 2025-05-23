56f6684c7f74fd357b56dca9e28e3807852b9c86
Nikhil Baisoya <nikhil_baisoya@bonify.de>
2025-05-23 18:24:24 +0530

    feat: fix guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/server')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/heartbeat')
+  getServerHeatbeat(): string {
+    return this.appService.getServerHeatbeat();
   }
 }
