7e6a1168240e7588a13abf146b3163ba49f5c9dd
Nikhil Baisoya <nikhil_baisoya@bonify.de>
2025-06-25 14:09:44 +0530

    feat: refactor code

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,2 @@
-export const jwtConstants = {
-  secret: 'puslyBackendTesting',
-  expiresIn: '30d',
-};
-
 export const GOOGLE_API_BASE_URL = 'https://maps.googleapis.com';
 export const DEVELOPMENT_HARD_CODED_OTP = '1234';
