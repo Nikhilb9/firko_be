bcab7f681f870191fa7df1a018246401c0c302c3
Nikhilb9 <48922298+Nikhilb9@users.noreply.github.com>
2025-06-27 15:36:22 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap(): Promise<void> {
     app.useGlobalPipes(new ValidationPipe());
 
     const options = new DocumentBuilder()
-      .setTitle('PUSLY API')
+      .setTitle('FIRKO API')
       .setVersion('1.0')
       .addServer('http://localhost:3000/', 'Local environment')
       .addBearerAuth(
